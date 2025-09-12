7edf250529d8a4c5d495fd2fc3570bf7e5365856
Lucas Francelino <lucas.francelino@academico.ifpb.edu.br>
2025-09-12 17:26:27 +0000

    json de municipios

--- backend/router.js
+++ backend/router.js
@@ -4,4 +4,8 @@ const express = require('express');
 const options = {}; 
 const router = express.Router(options);
 
+router.get('/municipios', (req, res) => {
+    res.json({ message: 'Lista de municípios' });
+});
+
 module.exports = router;
