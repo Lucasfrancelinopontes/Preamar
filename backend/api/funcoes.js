f55df121df391b0d241b99e9c68cb8085a0fec38
Lucas Francelino <lucas.francelino@academico.ifpb.edu.br>
2025-10-02 13:23:10 -0300

    rota de especies

--- backend/api/funcoes.js
+++ backend/api/funcoes.js
@@ -1,6 +1,12 @@
 import municipios from './municipios.json' with { type: 'json' };
+import especies from './especies.json' with { type: 'json' };
 
 export const gmun = async (req,res) => {
     console.log(municipios);
     res.json(municipios);
+}
+
+export const gesp = async (req,res) => {
+    console.log(especies);
+    res.json(especies);
 }
--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 const router = Router()
-import {gmun} from './api/funcoes.js'
+import { gmun, gesp } from './api/funcoes.js'
 
 router.get('/municipios', gmun)
+router.get('/especies', gesp)
 
 export default router;
