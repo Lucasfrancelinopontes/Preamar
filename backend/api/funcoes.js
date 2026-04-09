25786acdeda0d616c23511064ec0d0c2604f596b
lucas francelino <lucasfrancelinopontes@gmail.com>
2026-04-09 13:03:38 -0300

    refactor: get de especies

--- backend/api/funcoes.js
+++ backend/api/funcoes.js
@@ -1,5 +1,5 @@
 import municipios from './municipios.json' with { type: 'json' };
-import especies from './especies.json' with { type: 'json' };
+import { Especie } from '../models/index.js';
 
 export const gmun = async (req,res) => {
     console.log(municipios);
@@ -7,6 +7,47 @@ export const gmun = async (req,res) => {
 }
 
 export const gesp = async (req,res) => {
-    console.log(especies);
-    res.json(especies);
+    try {
+        const especies = await Especie.findAll({
+            order: [['nome_popular', 'ASC']],
+            attributes: [
+                'ID_especie',
+                'familia',
+                'nome_cientifico',
+                'nome_popular',
+                'genero',
+                'habitat',
+                'grau_ameaca',
+                'nivel_trofico',
+                'valor_comercial',
+                'mercado',
+                'comprimento_max_cm',
+                'inicio_maturacao_cm',
+                'pesca'
+            ]
+        });
+
+        const especiesFormatadas = especies.map((especie) => ({
+            ID: especie.ID_especie,
+            Familia: especie.familia,
+            Nome_cientifico: especie.nome_cientifico,
+            Nome_popular: especie.nome_popular,
+            Genero: especie.genero,
+            Habitat: especie.habitat,
+            Grau_de_ameaca: especie.grau_ameaca,
+            Nivel_trofico: especie.nivel_trofico,
+            Valor_comercial: especie.valor_comercial,
+            Mercado: especie.mercado,
+            Comprimento_max_cm: especie.comprimento_max_cm,
+            Inicio_maturacao_cm: especie.inicio_maturacao_cm,
+            Pesca: especie.pesca
+        }));
+
+        res.json(especiesFormatadas);
+    } catch (error) {
+        console.error('Erro ao buscar especies no banco:', error);
+        res.status(500).json({
+            message: 'Erro ao buscar especies'
+        });
+    }
 }
